e4e290f3eee2472ed54c59071ea4d8c9563ffd97
Seyyed Soroosh Hosseinalipour <soorosh_abi@hotmail.com>
2016-10-13 15:45:53 +0330

    Add call history changed task.

---
+++ CallHistoryChangedTest/CallHistoryChangedTest/CallHangupTask.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Calls.Background;
+using Windows.ApplicationModel.Calls.Provider;
+
+namespace CallHistoryChangedTest
+{
+    public sealed class CallHangupTask : IBackgroundTask
+    {
+        //
+        // The Run method is the entry point of a background task.
+        //
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Associate a cancellation handler with the background task.
+            // Even though this task isn't performing much work, it can still be cancelled.
+            taskInstance.Canceled += new BackgroundTaskCanceledEventHandler(OnCanceled);
+
+            // Do the background task activity.
+            BackgroundTaskDeferral _deferral = taskInstance.GetDeferral();
+
+            Toast.Send("CallHistoryChanged");
+
+            _deferral.Complete();
+        }
+
+        //
+        // Handles background task cancellation.
+        //
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            // Normally, we would signal the task to exit.
+            // However, our sample task has no async operations and is so short that
+            // we'll just let it run to completion instead of trying to cancel it.
+        }
+    }
+}
--- CallHistoryChangedTest/CallHistoryChangedTest/MainPage.xaml.cs
+++ CallHistoryChangedTest/CallHistoryChangedTest/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.Calls.Background;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +27,46 @@ namespace CallHistoryChangedTest
         public MainPage()
         {
             this.InitializeComponent();
+
+            if (BackgroundTaskRegistration.AllTasks.Count > 0)
+            {
+                RegisterBackgroundTask(typeof(CallHangupTask),
+                    new PhoneTrigger(PhoneTriggerType.CallOriginDataRequest, false));
+
+                Toast.Send("Background tasks is now registered");
+            }
+            else
+                Toast.Send("Registered background tasks");
+        }
+
+        private async void RegisterBackgroundTask(Type type, IBackgroundTrigger trigger)
+        {
+            BackgroundAccessStatus access = await BackgroundExecutionManager.RequestAccessAsync();
+            if (access == BackgroundAccessStatus.AlwaysAllowed ||
+                access == BackgroundAccessStatus.AllowedSubjectToSystemPolicy)
+            {
+                // If the task is already registered. then don't register it again.
+                foreach (IBackgroundTaskRegistration task in BackgroundTaskRegistration.AllTasks.Values)
+                {
+                    if (task.Name == type.Name)
+                    {
+                        return;
+                    }
+                }
+
+                var builder = new BackgroundTaskBuilder();
+
+                // Name the background task.
+                builder.Name = type.Name;
+
+                // Specify the background task to run when the trigger fires.
+                builder.TaskEntryPoint = type.FullName;
+
+                builder.SetTrigger(trigger);
+                BackgroundTaskRegistration taskRegistration = builder.Register();
+            }
+            else
+                Toast.Send("Background agent access denied");
         }
     }
 }
